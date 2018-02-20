5cacf5981a3d3a150b4da8ceff05432c1bfb5280
Madeline Wong <madelinegwong@outlook.com>
2018-02-20 10:29:20 -0500

    cleaned up the react app and added a basic idea of the sidebar.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserSideBar from './UserSideBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        pls
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/UserSideBar.js
@@ -0,0 +1,16 @@
+import './UserSideBar.html'
+import './UserSideBar.css'
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
+
+export default UserSideBar.js;
